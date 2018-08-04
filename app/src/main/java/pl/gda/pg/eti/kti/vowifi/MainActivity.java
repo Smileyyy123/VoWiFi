4446f5ce52da6cbc9ce43e5c953184a24dceafa0
Smileyyy123 <small_smile@interia.pl>
2018-08-04 20:35:18 +0200

    First commit - project files upload

---
+++ app/src/main/java/pl/gda/pg/eti/kti/vowifi/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.gda.pg.eti.kti.vowifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
